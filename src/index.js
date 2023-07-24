4020ecef0fd58d8ae321186568f90dafc14c4449
maberizk <michelle.aberizk@gmail.com>
2023-07-24 14:38:10 -0700

    fixed routing issue

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import Main from "./components/Main";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
+
+// const root = createRoot(document.getElementById("app"));
+
+// root.render(
+//     <Router basename="/">
+//         <Routes>
+//             <Route path="*" element={<Main />} />
+//         </Routes>
+//     </Router>
+// )
+
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Main from "./components/Main";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./components/Homepage";
+import Portfolio from "./components/Portfolio";
+import About from "./components/About";
+
+const Main = () => {
+    return (
+        <Router>
+            <div>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<Homepage />} />
+                        <Route path="/portfolio" element={<Portfolio />} />
+                        <Route path="/about" element={<About />} />
+                    </Routes>
+                </main>
+            </div>
+        </Router>
+    )
+}
 
 const root = createRoot(document.getElementById("app"));
-
-root.render(
-    <Router basename="/">
-        <Routes>
-            <Route path="*" element={<Main />} />
-        </Routes>
-    </Router>
-)
+root.render(<Main />)
