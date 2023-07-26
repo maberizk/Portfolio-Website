719de454163ea1bf06e769b8d53b2d6c3d7833f7
maberizk <michelle.aberizk@gmail.com>
2023-07-26 11:45:27 -0700

    set up data file to map images

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,80 @@
+// import React from 'react'
+// import Navbar from './Navbar'
+// import Footer from './Footer'
+// import neighborlyHome from '../../public/images/neighborlyHome.jpg'
+// import seasonsBloom from '../../public/images/seasonsBloom.png'
+// import powderBath from '../../public/images/powderBath.jpeg'
+// import { useState } from 'react'
+
+// export default function Portfolio() {
+//     const [hoveredProject, setHoveredProject] = useState(null);
+
+//     const handleMouseOver = (projectId) => {
+//         setHoveredProject(projectId);
+//     };
+
+//     const handleMouseOut = () => {
+//         setHoveredProject(null);
+//     };
+
+//     return (
+//         <div className="portfolio">
+//             <Navbar />
+//             <div className="portfolioProjects">
+//                 <div
+//                     className="projectContainer"
+//                     onMouseOver={() => handleMouseOver('neighborly')}
+//                     onMouseOut={handleMouseOut}
+//                 >
+//                     <a href="project.html">
+//                         <img src={neighborlyHome} className="project" alt="Neighborly Home" />
+//                     </a>
+//                     {hoveredProject === 'neighborly' && (
+//                         <div className="imageOverlay">
+//                             <h2 className="imageText">UX Design Concept - Neighborly</h2>
+//                         </div>
+//                     )}
+//                 </div>
+//                 <div
+//                     className="projectContainer"
+//                     onMouseOver={() => handleMouseOver('seasonsBloom')}
+//                     onMouseOut={handleMouseOut}
+//                 >
+//                     <a href="project.html">
+//                         <img src={seasonsBloom} className="project" alt="Seasons Bloom" />
+//                     </a>
+//                     {hoveredProject === 'seasonsBloom' && (
+//                         <div className="imageOverlay">
+//                             <h2 className="imageText">UX Design Concept - Seasons Bloom</h2>
+//                         </div>
+//                     )}
+//                 </div>
+//                 <div
+//                     className="projectContainer"
+//                     onMouseOver={() => handleMouseOver('powderBath')}
+//                     onMouseOut={handleMouseOut}
+//                 >
+//                     <a href="project.html">
+//                         <img src={powderBath} className="project" alt="Powder Bath" />
+//                     </a>
+//                     {hoveredProject === 'powderBath' && (
+//                         <div className="imageOverlay">
+//                             <h2 className="imageText">Residential Interior - Design and Project Management</h2>
+//                         </div>
+//                     )}
+
+//                 </div>
+//             </div>
+//             <Footer />
+//         </div>
+//     );
+// }
+
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
-import neighborlyHome from '../../public/images/neighborlyHome.jpg'
-import seasonsBloom from '../../public/images/seasonsBloom.png'
-import powderBath from '../../public/images/powderBath.jpeg'
 import { useState } from 'react'
+import { projects } from "../data"
 
 export default function Portfolio() {
     const [hoveredProject, setHoveredProject] = useState(null);
@@ -20,49 +90,23 @@ export default function Portfolio() {
     return (
         <div className="portfolio">
             <Navbar />
-            <div className="portfolioProjects">
-                <div
-                    className="projectContainer"
-                    onMouseOver={() => handleMouseOver('neighborly')}
-                    onMouseOut={handleMouseOut}
-                >
-                    <a href="project.html">
-                        <img src={neighborlyHome} className="project" alt="Neighborly Home" />
-                    </a>
-                    {hoveredProject === 'neighborly' && (
-                        <div className="imageOverlay">
-                            <h2 className="imageText">UX Design Concept - Neighborly</h2>
+            <div>
+                {projects.map((project) => (
+                    <a
+                        href={project.link}
+                        key={project.title}>
+                        <div>
+                            <img
+                                alt="gallery"
+                                src={project.image}
+                            />
+                            {console.log(project.image)}
+                            <h3>{project.title}</h3>
+                            <p>{project.description}</p>
                         </div>
-                    )}
-                </div>
-                <div
-                    className="projectContainer"
-                    onMouseOver={() => handleMouseOver('seasonsBloom')}
-                    onMouseOut={handleMouseOut}
-                >
-                    <a href="project.html">
-                        <img src={seasonsBloom} className="project" alt="Seasons Bloom" />
                     </a>
-                    {hoveredProject === 'seasonsBloom' && (
-                        <div className="imageOverlay">
-                            <h2 className="imageText">UX Design Concept - Seasons Bloom</h2>
-                        </div>
-                    )}
-                </div>
-                <div
-                    className="projectContainer"
-                    onMouseOver={() => handleMouseOver('powderBath')}
-                    onMouseOut={handleMouseOut}
-                >
-                    <a href="project.html">
-                        <img src={powderBath} className="project" alt="Powder Bath" />
-                    </a>
-                    {hoveredProject === 'powderBath' && (
-                        <div className="imageOverlay">
-                            <h2 className="imageText">Residential Interior - Design and Project Management</h2>
-                        </div>
-                    )}
-                </div>
+                ))}
+
             </div>
             <Footer />
         </div>
---
+++ src/data.js
@@ -0,0 +1,21 @@
+import neighborlyHome from '../public/images/neighborlyHome.jpg'
+import seasonsBloom from '../public/images/seasonsBloom.png'
+import powderBath from '../public/images/powderBath.jpeg'
+
+export const projects = [
+    {
+        title: "Neighborly",
+        description: "UX Design Concept",
+        image: "/images/neighborlyHome.jpg",
+    },
+    {
+        title: "Seasons Bloom",
+        description: "UX Design Concept",
+        image: '/images/seasonsBloom.png'
+    },
+    {
+        title: "Interior Design",
+        description: "Residential and Commercial",
+        image: '/images/powderBath.jpg'
+    }
+]
