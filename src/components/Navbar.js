93b74a9ed56ea4474684254a9fac62d751d69e32
maberizk <michelle.aberizk@gmail.com>
2023-08-08 11:02:21 -0700

    mobile responsive nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 const Navbar = () => {
     const location = useLocation();
     const isHomepage = location.pathname === "/";
+
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
         <nav data-aos="fade-down" className={isHomepage ? "nav" : "navWhite"}>
@@ -13,10 +15,22 @@ const Navbar = () => {
                     className={`${isHomepage ? "link" : "linkWhite"}`}
                 >Michelle Aberizk</Link></h1>
             </div>
-            <div className="navRight">
-                <h1 ><Link to="/portfolio" className={`${isHomepage ? "link" : "linkWhite"}`} >Portfolio</Link></h1>
-                <h1><Link to="/about" className={`${isHomepage ? "link" : "linkWhite"}`}>About</Link></h1>
+
+            <div className={`navRight ${isOpen ? "active" : ""}`}>
+                <h1>
+                    <Link to="/portfolio" className={`${isHomepage ? "link" : "linkWhite"}`} >
+                        Portfolio
+                    </Link></h1>
+
+                <h1>
+                    <Link to="/about" className={`${isHomepage ? "link" : "linkWhite"}`}>
+                        About</Link></h1>
             </div>
+            <a href="#" className={`toggle-button ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+                <span className={`bar ${isHomepage ? "" : "barWhite"}`}></span>
+                <span className={`bar ${isHomepage ? "" : "barWhite"}`}></span>
+                <span className={`bar ${isHomepage ? "" : "barWhite"}`}></span>
+            </a>
         </nav >
     )
 }
