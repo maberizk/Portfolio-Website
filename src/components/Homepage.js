43699b96d37a18c2425b709d3b8067313041eaca
maberizk <michelle.aberizk@gmail.com>
2023-08-15 13:40:07 -0700

    removed homepage animation

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -9,41 +9,41 @@ import oceanImg from "../../public/images/Ocean.jpg";
 
 
 const Homepage = () => {
-    const animationRef = useRef(null);
-    useEffect(() => {
+    // const animationRef = useRef(null);
+    // useEffect(() => {
 
-        let animation = document.querySelector(".animateText")
+    //     let animation = document.querySelector(".animateText")
 
-        // turning each letter into its own span
-        animation.innerHTML = animation.textContent.replace(/(\S)/g, "<span class='letter'>$1</span>");
-        {
-            console.log(animation)
-        }
-        // vertical movement 
-        animationRef.current = anime.timeline({ loop: true })
-            .add({
-                targets: '.animateText .letter',
-                translateY: [100, 0],
-                translateZ: 0,
-                opacity: [0, 1],
-                easing: "easeOutExpo",
-                duration: 1600,
-                delay: (el, i) => 300 + 30 * i
-            }).add({
-                targets: '.animateText .letter',
-                translateY: [0, -100],
-                opacity: [1, 0],
-                easing: "easeInExpo",
-                duration: 1400,
-                delay: (el, i) => 100 + 30 * i
-            });
-        return () => {
-            if (animationRef.current) {
-                animationRef.current.pause();
-                animationRef.current = null;
-            }
-        }
-    }, [])
+    //     // turning each letter into its own span
+    //     animation.innerHTML = animation.textContent?.replace(/(\S)/g, "<span class='letter'>$1</span>");
+    //     {
+    //         console.log(animation)
+    //     }
+    //     // vertical movement 
+    //     animationRef.current = anime.timeline({ loop: true })
+    //         .add({
+    //             targets: '.animateText .letter',
+    //             translateY: [100, 0],
+    //             translateZ: 0,
+    //             opacity: [0, 1],
+    //             easing: "easeOutExpo",
+    //             duration: 1600,
+    //             delay: (el, i) => 300 + 30 * i
+    //         }).add({
+    //             targets: '.animateText .letter',
+    //             translateY: [0, -100],
+    //             opacity: [1, 0],
+    //             easing: "easeInExpo",
+    //             duration: 1400,
+    //             delay: (el, i) => 100 + 30 * i
+    //         });
+    //     return () => {
+    //         if (animationRef.current) {
+    //             animationRef.current.pause();
+    //             animationRef.current = null;
+    //         }
+    //     }
+    // }, [])
 
 
 
